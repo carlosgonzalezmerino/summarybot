14db263ffeb3bf9d57a35f75608caaadb6d3211b
Lucas Menendez <epucas@gmail.com>
2017-09-28 10:56:24 +0200

    new database

--- assets/schema.sql
+++ assets/schema.sql
@@ -1,1 +1,2 @@
-CREATE TABLE `auths` (`team_id`	VARCHAR,`bot_token`	VARCHAR, PRIMARY KEY(`team_id`,`bot_token`));
+create table auths (team_id VARCHAR, bot_token VARCHAR, constraint auths_pk primary key (team_id, bot_token));
+create table news (id INTEGER primary key autoincrement, title TEXT, summary TEXT, keywords TEXT, url TEXT);
