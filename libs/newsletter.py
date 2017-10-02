e1f93a563fde2d87fc45fc824341adbae3dff3ec
Lucas Menendez <epucas@gmail.com>
2017-10-02 15:55:13 +0200

    Date limits

--- libs/newsletter.py
+++ libs/newsletter.py
@@ -9,41 +9,53 @@ class Newsletter(object):
 	def __init__(self, access_token):
 		self.access_token = access_token
 
+	def __getchannels(self):
+		channels = []
+
+		client = SlackClient(self.access_token)
+		response = client.api_call("channels.list")
+		if response and response.get("ok"):
+			for channel in response.get("channels"):
+				channels.append({
+					"id": channel.get("id"),
+					"name": channel.get("name")
+				})
+
+		response = client.api_call("groups.list")
+		if response and response.get("ok"):
+			for group in response.get("groups"):
+				channels.append({
+					"id": group.get("id"),
+					"name": group.get("name")
+				})
+
+		return channels or None
+
+	def __getkeywords(self, channels):
+		links = []
+		for channel in channels:
+			links += db.getAll("news", "channel_id", channel.get("id"))
+
+		keywords = {}
+		end = datetime.today() - timedelta(days=datetime.today().weekday())
+		start = end - timedelta(days=7)
+		for link in links:
+			if end > link.get("date") >= start:
+				link_keywords = link.get("keywords").split(",")
+
+				for keyword in link_keywords:
+					if keyword in keywords.keys():
+						keywords[keyword].append(link)
+					else:
+						keywords[keyword] = [link]
+
+		return keywords or None
+
 	def gettopics(self):
 		try:
-			channels = []
-
-			client = SlackClient(self.access_token)
-			response = client.api_call("channels.list")
-			if response and response.get("ok"):
-				for channel in response.get("channels"):
-					channels.append({
-						"id": channel.get("id"),
-						"name": channel.get("name")
-					})
-
-			response = client.api_call("groups.list")
-			if response and response.get("ok"):
-				for group in response.get("groups"):
-					channels.append({
-						"id": group.get("id"),
-						"name": group.get("name")
-					})
-
-			links = []
-			for channel in channels:
-				links += db.getAll("news", "channel_id", channel.get("id"))
-
-			keywords = []
-			end = datetime.today() - timedelta(days=datetime.today().weekday())
-			start = end - timedelta(days=7)
-			for link in links:
-				if end > link.get("date") >= start:
-					keywords += link.get("keywords").split(",")
-
-			print(start, end)
-			print(keywords)
-			return channels
+			channels = self.__getchannels()
+			if channels:
+				return self.__getkeywords(channels)
 		except Exception as e:
 			print(e)
 
