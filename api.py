c8749daaeb07b5ae8e802a14f5607ff244c36d8e
Lucas Menendez <epucas@gmail.com>
2017-10-02 17:00:23 +0200

    Check links number

--- api.py
+++ api.py
@@ -119,6 +119,7 @@ def newsletter(data):
 def topic(data, tag):
 	nw = Newsletter(data.get("access_token"))
 	links = nw.getlinks(tag)
+	print(links)
 	return render_template("newsletter/topic.html", data=data, topic=tag, links=links)
 
 
