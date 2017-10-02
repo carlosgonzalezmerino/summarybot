e4e0bf9be2d4628b7307fcbc344743d8db16c63d
Lucas Menendez <epucas@gmail.com>
2017-10-02 13:15:24 +0200

    Geetting channels

--- libs/database.py
+++ libs/database.py
@@ -147,6 +147,7 @@ class DB(object):
 			query = "%s WHERE %s=\'?\'" % (query, key)
 
 		try:
+			print(query, value)
 			self.cursor.execute(query, value)
 			results = self.cursor.fetchall()
 		except sqlite3.Error as err:
