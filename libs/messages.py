1a01b935ad74f7592047949d97c9d4ec060eb8e2
Lucas Menendez <epucas@gmail.com>
2017-10-17 12:11:23 +0200

    Bot messages issue fixed

--- libs/messages.py
+++ libs/messages.py
@@ -1,4 +1,4 @@
-INTRO="¿Cómo se halla hoy Vuesa Merced? Me llamo *Reverte Bot* y aunque ayer anduve un rato por mi biblioteca en la parte ocupada por clasicos griegos, hoy transito hacia este foro para *ayudar a extraer lo mejor de las noticias or articulos que aquí se viertan*."
+INTRO="¿Cómo se halla hoy Vuesa Merced? Me llamo *Reverte Bot* y aunque ayer anduve un rato por mi biblioteca en la parte ocupada por clasicos griegos, hoy transito hacia este foro para *ayudar a extraer lo mejor de las noticias o articulos que aquí se viertan*."
 CONTENT_NOMENTION_MSG="Sabiendo que nadie me ha dado vela en este entierro, le otorgo mi sabiduría sobre lo que acaba de exponer."
 CONTENT_MSG="En estas líneas resumo de forma escueta el contenido de su enlace."
 NO_URL="Muy señor mío, el mensaje debe incluir un enlace o  pagará caro ocupar mi tiempo de esta manera."
