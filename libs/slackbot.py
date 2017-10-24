614377021843f8b2104e2ed9765cf6bac29c0d2e
Lucas Menendez <epucas@gmail.com>
2017-10-24 13:07:40 +0200

    Scrapping BBVA blog

--- libs/slackbot.py
+++ libs/slackbot.py
@@ -102,8 +102,8 @@ class SlackBot(object):
 			response = requests.get(url)
 			response.raise_for_status()
 
-			body = response.text.decode('latin1').encode('utf8')
-			soup = BeautifulSoup(body, 'lxml')
+			body = response.text.encode('utf8')
+			soup = BeautifulSoup(body, 'lxml', from_encoding='utf8')
 			if "bbva.com" not in url:
 				article = self.__parsecontent(soup)
 			else:
