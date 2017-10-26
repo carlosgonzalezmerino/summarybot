333732f183b0724048bb900568568f778afdecae
Jesús Martín <jesus.martin@beeva.com>
2017-10-26 12:20:40 +0200

    english messages

---
+++ libs/messages-landing.py
@@ -0,0 +1,23 @@
+# error.html
+Opss... Vaya! -- Opss...
+Ha habido un error y Reverte Bot no se ha podido instalar correctamente. -- There was an error and Reverte hasn't been installed
+"Sin nada mas que decir, y pidiendo perdón, le envio un cordial saludo" -- "With nothing else to add I begging your pardon. Yours sincereley"
+
+# header.html
+Bienvenido - Welcome
+
+# index
+El bot resumidor que te habla de usted -- The summarizer bot that talks to you kindly in Slack
+"Soy el perfecto resumidor de mi cuñado para cualquier noticia que pase sin leerse." -- "I might say that I am the perfect summarizer for any article you prefer to read faster"
+
+# layout
+Reverte Bot - El bot resumidor que te habla de usted. -- Reverte Bot - The summarizer bot that talks to you kindly in Slack
+
+# login
+No estas autenticado! -- You are not authenticated!
+Accede con tu cuenta y podrás ver un resumen de todo lo que han mandado en tu Slack. -- Access with your personal account to get the summaries from anything published in your Slack
+
+# thanks
+Gracias! -- Thank you!
+Reverte Bot se ha instalado correctamente -- Reverte Bot was successfully installed
+"Gracias por la compañía y por las cañas." -- "Thanks for the company and the drinks"
